33922f6fec27148a5b073609c5965840b02fb6b9
Bruno Chiconato <brunochiconato01@gmail.com>
2024-09-27 21:14:00 -0300

    Update
    - Exercício 3 de conexão com banco de dados realizado na pasta 'projeto_banco_dados' finalizado.
    
    - Foi utlizado um banco Postgres juntamente com Docker.

---
+++ projeto_banco_dados/src/db_operations.py
@@ -0,0 +1,130 @@
+import psycopg2 # type: ignore
+
+def connect_postgres():
+    """
+    Estabelece uma conexão com o banco de dados PostgreSQL.
+
+    Tenta conectar ao banco de dados PostgreSQL com as credenciais e configurações fornecidas.
+    Se a conexão for bem-sucedida, retorna o objeto de conexão. Caso contrário, retorna None.
+
+    :return: Objeto de conexão ao banco de dados PostgreSQL se a conexão for bem-sucedida,
+    ou None em caso de falha.
+    """
+    try:
+        conexao = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            database="postgres",
+            user="postgres",
+            password="postgres"
+        )
+        print("Conexão com o banco de dados foi bem-sucedida!")
+        return conexao
+
+    except Exception as erro:
+        print(f"Erro ao conectar ao banco de dados: {erro}")
+        return None
+
+def db_create_table(conexao):
+    """
+    Cria a tabela 'funcionarios' no banco de dados, se ela não existir.
+
+    Executa um comando SQL para criar uma tabela chamada 'funcionarios' com colunas de ID, nome e departamento.
+    A função faz o commit da transação ao banco de dados e fecha a conexão ao final.
+
+    :params: Objeto de conexão ao banco de dados PostgreSQL.
+
+    :return: None
+    """
+    if conexao is None:
+        print("Conexão inválida.")
+        return
+
+    try:
+        cursor = conexao.cursor()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS funcionarios(
+                id SERIAL PRIMARY KEY,
+                nome VARCHAR(100) NOT NULL,
+                departamento VARCHAR(100) NOT NULL
+            );
+        """)
+
+        conexao.commit()
+
+        print("Tabela criada com sucesso!")
+
+    except Exception as erro:
+        print(f"Erro ao criar a tabela: {erro}")
+
+def db_insert_data(conexao):
+    """
+    Insere dados na tabela 'funcionarios'.
+
+    Insere cinco registros de funcionários com os nomes e departamentos especificados na tabela 'funcionarios'.
+    Faz o commit da transação e fecha a conexão ao banco de dados.
+
+    :params: Objeto de conexão ao banco de dados PostgreSQL.
+
+    :return: None
+    """
+    if conexao is None:
+        print("Conexão inválida.")
+        return
+
+    try:
+        cursor = conexao.cursor()
+
+        cursor.execute("""
+            INSERT INTO funcionarios (nome, departamento) VALUES 
+            ('Bruno', 'Vendas'),
+            ('Gabriel', 'Compras'),
+            ('Gustavo', 'Compras'),
+            ('Fabrício', 'Peças'),
+            ('Pablo', 'Vendas');
+        """)
+
+        conexao.commit()
+
+        print("Dados inseridos com sucesso!")
+
+    except Exception as erro:
+        print(f"Erro ao tentar inserir dados na tabela: {erro}")
+
+def db_retrieve_data(conexao):
+    """
+    Recupera dados da tabela 'funcionarios' onde o departamento é 'Vendas'.
+
+    Executa uma consulta SQL para buscar os nomes dos funcionários que pertencem ao departamento 'Vendas'.
+    Retorna os resultados da consulta.
+
+    :params: Objeto de conexão ao banco de dados PostgreSQL.
+
+    :params: Lista de tuplas com os nomes dos funcionários que pertencem ao departamento 'Vendas'.
+    """
+    if conexao is None:
+        print("Conexão inválida.")
+        return
+
+    try:
+        cursor = conexao.cursor()
+
+        cursor.execute("""
+            SELECT nome FROM funcionarios
+            WHERE departamento = 'Vendas';
+        """)
+
+        resultado = cursor.fetchall()
+
+        print("Consulta realizada com sucesso!")
+
+    except Exception as erro:
+        print(f"Erro ao tentar inserir dados na tabela: {erro}")
+    
+    finally:
+        cursor.close()
+        conexao.close()
+        print("Conexão encerrada.")
+
+        return resultado
---
+++ projeto_banco_dados/src/main.py
@@ -0,0 +1,17 @@
+from db_operations import connect_postgres, db_create_table, db_insert_data, db_retrieve_data
+
+def main():
+    conexao = connect_postgres()
+
+    #db_create_table(conexao)
+    #db_insert_data(conexao)
+
+    resultado = db_retrieve_data(conexao)
+
+    print("Os funcionários do departamento de 'Vendas' são:")
+    [print(f"- {tupla[0]}") for tupla in resultado]
+
+
+
+if __name__ == '__main__':
+    main()
